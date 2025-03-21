17222d6dfabf4e3ecbc271ca334fd36122eeb2b0
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-03-21 13:48:07 +0100

    basics done

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,15 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-function fn(x) {
-    return x.flip();
-}
-console.log(typeof fn);
+const ODirection = {
+    Up: 0,
+    Down: 1,
+    Left: 2,
+    Right: 3,
+};
+console.log(1 /* EDirection.Down */, ODirection.Down);
+// Using the enum as a parameter
+function walk(dir) { }
+function run(dir) { }
+walk(2 /* EDirection.Left */);
+run(ODirection.Right);
 //# sourceMappingURL=app.js.map
