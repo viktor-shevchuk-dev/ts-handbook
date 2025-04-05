f06f10e50ef06e87a04edd3b1f6105165bd12101
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-04-05 16:06:20 +0200

    non-obligatory params

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,138 @@
 'use strict';
 
-// function padLeft(padding: number | string, input: string): string {
-//   return ' '.repeat(padding) + input;
-//   // Argument of type 'string | number' is not assignable to parameter of type 'number'.
-//   // Type 'string' is not assignable to type 'number'.
+// function greeter(fn: (a: string) => void) {
+//   fn('Hello world!');
+// }
+// function printToConsole(str: string) {
+//   console.log(str);
+// }
+// greeter(printToConsole);
+
+// type GreetFunction = (a: string) => void;
+// function greeter(fn: GreetFunction) {
+//   fn('Hello world!');
+// }
+// function printToConsole(str: string) {
+//   console.log(str);
+// }
+// greeter(printToConsole);
+
+// type DescribableFunction = {
+//   description: string;
+//   (someArg: number): boolean;
+// };
+// function doSth(fn: DescribableFunction) {
+//   console.log(fn.description + ' returned ' + fn(6));
+// }
+// function myFunc(someArg: number) {
+//   return someArg > 3;
+// }
+// myFunc.description = 'default description';
+// doSth(myFunc);
+
+// type SomeObject = {};
+// type SomeConstructor = { new (s: string): SomeObject };
+// function fn(ctor: SomeConstructor) {
+//   return new ctor('hello');
 // }
 
-// function padLeft(padding: number | string, input: string): string {
-//   if (typeof padding === 'number') {
-//     return ' '.repeat(padding) + input;
-//   }
-//   return padding + input;
+// type CallOrConstruct = {
+//   (n?: number): string;
+//   new (s: string): Date;
+// };
+// interface CallOrConstruct {
+//   (n?: number): string;
+//   new (s: string): Date;
+// }
+// function fn(ctor: CallOrConstruct) {
+//   console.log(ctor(10));
+//   console.log(new ctor('10'));
+// }
+// fn(Date);
+
+// function firstElement(arr:any[]){
+//   return arr[0]
 // }
 
-// function printAll(strs: string | string[] | null) {
-//   if (typeof strs === 'object') {
-//     for (const s of strs) {
-//       // 'strs' is possibly 'null'.
-//       console.log(s);
-//     }
-//   } else if (typeof strs === 'string') {
-//     console.log(strs);
+// function firstElement<Type>(arr: Type[]): Type | undefined {
+//   return arr[0];
+// }
+// const s = firstElement(['a', 'b']);
+// const u = firstElement([]);
+
+// function map<Input, Output>(
+//   arr: Input[],
+//   func: (arg: Input) => Output
+// ): Output[] {
+//   return arr.map(func);
+// }
+// const parsed = map(['1', '2'], (n) => parseInt(n));
+
+// function longest<Type extends { length: number }>(a: Type, b: Type) {
+//   return a.length >= b.length ? a : b;
+// }
+// const longerArr = longest([1, 2], [1, 2, 3]);
+// const longerStr = longest('alice', 'bob');
+// // const notOK = longest(10, 100);
+// console.log({
+//   longerArr,
+//   longerStr,
+//   // notOK
+// });
+
+// function minimumLength<Type extends { length: number }>(
+//   obj: Type,
+//   minimum: number
+// ): Type {
+//   if (obj.length >= minimum) {
+//     return obj;
 //   } else {
-//     // do nothing
+//     return { length: minimum };
 //   }
 // }
+// // 'arr' gets value { length: 6 }
+// const arr = minimumLength([1, 2, 3], 6);
+// // and crashes here because arrays have
+// // a 'slice' method, but not the returned object!
+// console.log(arr.slice(0));
 
-// function getUsersOnlineMessage(numUsersOnline: number) {
-//   if (numUsersOnline) {
-//     return `There are ${numUsersOnline} online now!`;
-//   }
-//   return "Nobody's here. :(";
+// function combine<Type>(arr1: Type[], arr2: Type[]) {
+//   return arr1.concat(arr2);
+// }
+// const arr = combine<string | number>([1, 2, 3], ['hello']);
+// console.log(arr);
+
+// function firstElement1<Type>(arr: Type[]) {
+//   return arr[0];
+// }
+// function firstElement2<Type extends any[]>(arr: Type) {
+//   return arr[0];
+// }
+// const a = firstElement1([1, 2, 3]);
+// const b = firstElement2([1, 2, 3]);
+
+// function filter1<Type>(arr: Type[], func: (arg: Type) => boolean) {
+//   return arr.filter(func);
+// }
+// function filter2<Type, Func extends (arg: Type) => boolean>(
+//   arr: Type[],
+//   func: Func
+// ) {
+//   return arr.filter(func);
 // }
 
-// both of these result in 'true'
-// Boolean('hello'); // type: boolean, value: true
-// !!'world'; // type: true,    value: true
-
-// type Fish = { swim: () => void };
-// type Bird = { fly: () => void };
-// function move(animal: Fish | Bird) {
-//   if ('swim' in animal) {
-//     return animal.swim();
-//   }
-
-//   return animal.fly();
+// function greet1<Str extends string>(s: Str) {
+//   console.log('Hey, ' + s);
 // }
+// greet1('world');
+
+// function greet2(s: string) {
+//   console.log('Hello, ' + s);
+// }
+
+// function f(n: number = 10) {
+//   console.log(n?.toFixed());
+//   console.log(n?.toFixed(3));
+// }
+// f();
+// f(10.9999);
